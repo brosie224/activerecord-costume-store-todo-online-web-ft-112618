b479bfdbb7746d4d4eb0dc67a3ed2933062fa9e4
Brian Rosengarten <barosengarten@gmail.com>
2019-01-29 01:38:41 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
         t.string :name
         t.float :price
         t.string :size
-        t.tring :image_url
+        t.string :image_url
         t.timestamps
       end
     end
